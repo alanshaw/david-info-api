e3f05e3e3f4961a0235ae92fcd6fd063a68abda2
Alan Shaw <alan.shaw@protocol.ai>
2020-12-13 14:56:13 +0000

    fix: config prefix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const MINUTE = 1000 * 60
 const HOUR = MINUTE * 60
 const DAY = HOUR * 24
 
-export default rc('info-api', {
+export default rc('david', {
   port: 3000,
   cache: {
     type: 'memory', // also 'redis' + add additional redisConnectionString and ioredis prop for config
